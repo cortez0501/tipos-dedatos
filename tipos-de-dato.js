9c1e86bc68a5184fe214cfa9f29670c474b3bf33
Mafer Cortez <maferc20190501@gmail.com>
2024-10-16 16:45:42 -0500

    Add files via upload

---
+++ tipos-de-dato.js
@@ -0,0 +1,36 @@
+console.log("hola mundo")
+
+let numero = 42
+
+console.log(typeof numero)
+
+console.log(numero)
+
+let miVariable = 'Veinticinco'
+
+console.log (typeof miVariable)
+
+console.log(25+25)
+
+console.log (1-1)
+
+console.log(false==4)
+
+console.log(true==1)
+
+MiVariable = null
+
+console.log(MiVariable)
+
+MiVariable = undefined
+
+console.log(MiVariable)
+
+let numeroNegativo = -666
+
+console.log(numeroNegativo)
+
+
+
+
+
